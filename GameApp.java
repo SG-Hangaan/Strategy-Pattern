cc68e46246ceec64d0175e15a1caa1f87bbae013
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2024-02-18 00:31:56 +0800

    Delete GameApp.java

--- GameApp.java
+++
@@ -1,7 +0,0 @@
-public class GameApp{
-    public static void main(String [] args){
-
-        Command command = new Command (usingKnight, new DefenseStrategy());
-        System.out.println(Command.characterType);
-    }
-}
